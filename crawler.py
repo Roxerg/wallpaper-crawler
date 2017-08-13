ce49f269a6f1af00f66f4f05344ed1bc8174a04a
Rokas Gudavicius <roxguda@gmail.com>
2017-08-13 18:27:41 +0100

    url prep setup

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import random
 
-url = "https://www.jpl.nasa.gov/spaceimages/images/wallpaper/PIA07220-1920x1200.jpg"
 
-# PIA07220 to be randomly changed. 
-# find out the amount of randomness accepted
+num = "%0.5d" % random.randint(0, 210000)
+res = "1920x1200"  # make adjustable res
+url ="https://www.jpl.nasa.gov/spaceimages/images/wallpaper/PIA" + num + "-" + res ".jpg"
+
