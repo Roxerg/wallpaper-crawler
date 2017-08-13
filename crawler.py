5c6d787bb9dba91f65213c2926619903c8189aab
Rokas Gudavicius <roxguda@gmail.com>
2017-08-13 17:56:09 +0100

    initial commit

---
+++ crawler.py
@@ -0,0 +1,6 @@
+import random
+
+url = "https://www.jpl.nasa.gov/spaceimages/images/wallpaper/PIA07220-1920x1200.jpg"
+
+# PIA07220 to be randomly changed. 
+# find out the amount of randomness accepted
